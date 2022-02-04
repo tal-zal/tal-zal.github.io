b33bdae05a26c6b7ced45d55c718357bb6ed13f8
talzaloilov <tzaloilo@sfu.ca>
2022-02-04 11:43:14 -0800

    Hero Page Done

---
+++ script.js
@@ -0,0 +1,22 @@
+// document.getElementById("hi").addEventListener("mouseover", (self) => {
+//     self.textContent = "About";
+// })
+
+
+document.querySelector(".heroTextTyping").addEventListener("animationend", () => {
+    
+    setTimeout(() => {    
+
+    let navigation = document.querySelector(".heroNavigation");
+    navigation.style.color = "grey";
+    
+    let aboutNav = document.getElementById("about");
+    let projectsNav = document.getElementById("projects");
+    let contactNav = document.getElementById("contact");
+
+    aboutNav.id = "aboutDone";
+    projectsNav.id = "projectsDone";
+    contactNav.id = "contactDone";
+
+    }, 200)
+});
